0854ea8a903a70826058b0a87a91eb712f81b572
jamerson Macedo <jamerson_macedo@hotmail.com>
2023-10-11 16:11:19 -0300

    scroolview configurado

--- app/src/main/java/com/example/instaclone/view/HomeScreen.kt
+++ app/src/main/java/com/example/instaclone/view/HomeScreen.kt
@@ -1,18 +1,18 @@
 package com.example.instaclone.view
 
-import android.content.ClipData.Item
+
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.instaclone.data.model.Feed
@@ -20,22 +20,21 @@ import com.example.instaclone.data.model.Story
 import com.example.instaclone.data.repository.stories
 import com.example.instaclone.ui.theme.dividerColor
 import com.example.instaclone.ui.theme.spacingMedium
+import com.example.instaclone.data.repository.feedList
 
 @Composable
 fun HomeScreen() {
 // como se fosse o linear layout
     // fill max size deixa do tamanho da tela
     // deixando compativel com o modo escuro
-    Column(
+    LazyColumn(
         modifier = Modifier
-            .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
     ) {
-        InstaToolbar()
-        storyList(story = stories)
-        Divider(color = dividerColor, thickness = 0.2.dp)
-        feedList(feed = com.example.instaclone.data.repository.feedList)
-
+        item { InstaToolbar() }
+        item { storyList(story = stories) }
+        item { Divider(color = dividerColor, thickness = 0.2.dp) }
+        feedlist(feed = feedList)
 
     }
 }
@@ -55,21 +54,15 @@ fun storyList(story: List<Story>) {
 
 }
 
-@Composable
-fun feedList(feed: List<Feed>) {
-    LazyColumn(modifier = Modifier.padding(top = spacingMedium)) {
-        // funciona como se fosse um adapter
-        // o item do story
-        // item(feed) { _, item ->
-        itemsIndexed(feed) { _,item->
+fun LazyListScope.feedlist(feed: List<Feed>) {
+
+        itemsIndexed(feed) { _, item ->
             FeedItem(feed = item)
         }
 
 
     }
 
-
-}
 
 @Preview(showBackground = true)
 @Composable
